66e3f0914fb21fcaa7a55d87efa323b75fbc6cce
lirik00199 <asktrct@gmail.com>
2025-11-13 20:04:50 +0300

    Initial commit: project setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center">
+    <h1 class="text-5xl font-bold animate__animated animate__bounce">
+      <i class="fas fa-cat"></i>
+      Cat-Pac
+    </h1>
+    <p class="mt-4 text-lg">Project setup is complete. Ready to build!</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/main.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'animate.css'
+
+createApp(App).mount('#app')
